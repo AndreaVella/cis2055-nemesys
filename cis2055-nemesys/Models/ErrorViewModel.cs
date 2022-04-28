6dcc77a9568d679ebd0bf9ab6b15715362d3e732
AndreaVella <82958930+AndreaVella@users.noreply.github.com>
2022-04-28 19:54:24 +0200

    Initial Creation

---
+++ cis2055-nemesys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace cis2055_nemesys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
